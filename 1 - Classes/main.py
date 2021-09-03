1a7d4feb6b00eaa66e90132e0dbd6cf67713932a
Pedro Lisboa <phlisboa2000@gmail.com>
2021-09-02 21:01:55 -0300

    Começando POO

---
+++ 1 - Classes/main.py
@@ -0,0 +1,23 @@
+from pessoa import Pessoa
+
+p1 = Pessoa('Pedro', 28,)
+p2 = Pessoa('João', 32)
+p3 = Pessoa('Marcelo', 15)
+
+p1.comer('banana')
+p1.comer('banana')
+
+p1.para_de_comer()
+p1.para_de_comer()
+
+p1.comer('macarrão')
+p1.falar('Carnaval')
+p1.para_de_comer()
+p1.falar('Carnaval')
+p1.falar('Carnaval')
+p1.para_de_falar()
+p1.para_de_falar()
+
+
+
+print(p2.get_ano_nascimento())
